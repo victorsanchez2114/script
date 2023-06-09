083b8faaa5292a5738c63734a453723a0f16baa3
victorsanchez2114.vs <victorsanchez2114.vs@gmail.com>
2023-06-09 17:40:35 -0500

    script 1

---
+++ js/main.js
@@ -0,0 +1,14 @@
+//guardar el estado columna activa() 
+let columnaActiva = 1
+//seleccionamos las columnas
+
+const columnas= document.querySelectorAll(".columna")
+//escuchar los clicks en cada columna
+//columnas.forEach((columna,indice) ⇒ {
+//columna.addEventListener("click",funtion(){
+  //console.log(columna,indice)
+
+//})
+  
+
+//})
